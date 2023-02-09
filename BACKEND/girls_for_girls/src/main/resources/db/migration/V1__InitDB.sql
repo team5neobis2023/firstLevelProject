eca23ad08cdd0d8457668d1dd951f45bf5302ec6
umar <UDzhumaza@beeline.kg>
2023-02-09 16:43:53 +0600

    Fix script

--- BACKEND/girls_for_girls/src/main/resources/db/migration/V1__InitDB.sql
+++ BACKEND/girls_for_girls/src/main/resources/db/migration/V1__InitDB.sql
@@ -1,13 +1,24 @@
-DROP SCHEMA IF EXISTS g4g_db;
-CREATE SCHEMA g4g_db;
 
-CREATE TABLE g4g_db.user_group(
-    id serial not null,
+CREATE TABLE user_group(
+    id serial primary key,
     name varchar
 );
 
-CREATE TABLE g4g_db.user(
-    ID serial not null,
+CREATE TABLE file(
+    id serial primary key,
+    rec_time timestamp default now(),
+    name varchar,
+    file_code varchar,
+    is_deleted boolean default false
+);
+
+CREATE TABLE product_group(
+    id serial primary key,
+    name varchar
+);
+
+CREATE TABLE users(
+    id serial primary key,
     rec_time timestamp default now(),
     email varchar,
     password varchar,
@@ -15,12 +26,12 @@ CREATE TABLE g4g_db.user(
     phone_number varchar,
     group_id int not null,
     file_id int,
-    CONSTRAINT fk_group FOREIGN KEY(group_id) REFERENCES user_group(id),
-    CONSTRAINT fk_file FOREIGN KEY(file_id) REFERENCES file(id)
+    FOREIGN KEY(group_id) REFERENCES user_group(id),
+    FOREIGN KEY(file_id) REFERENCES file(id)
 );
 
-CREATE TABLE g4g_db.article (
-    Id serial not null,
+CREATE TABLE article (
+    id serial primary key,
     rec_time timestamp default now(),
     update_time timestamp,
     title varchar,
@@ -29,40 +40,27 @@ CREATE TABLE g4g_db.article (
     like_id int,
     user_id int not null,
     file_id int,
-    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES user(id),
-    CONSTRAINT fk_user_like FOREIGN KEY(like_id) REFERENCES user(id),
-    CONSTRAINT fk_file FOREIGN KEY(file_id) REFERENCES file(id)
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(like_id) REFERENCES users(id),
+    FOREIGN KEY(file_id) REFERENCES file(id)
 );
 
-CREATE TABLE g4g_db.product(
-    id serial not null,
+CREATE TABLE product(
+    id serial primary key,
     name varchar,
     description varchar,
     price int,
     size varchar,
     product_group_id int,
     file_id int,
-    CONSTRAINT fk_product_group FOREIGN KEY(product_group_id) REFERENCES product_group(id),
-    CONSTRAINT fk_file FOREIGN KEY(file_id) REFERENCES file(id)
+    FOREIGN KEY(product_group_id) REFERENCES product_group(id),
+    FOREIGN KEY(file_id) REFERENCES file(id)
 );
 
-CREATE TABLE g4g_db.product_group(
-    id serial not null,
-    name varchar
-);
-
-CREATE TABLE g4g_db.order(
-    user_id int REFERENCES user (id) ON UPDATE CASCADE ON DELETE CASCADE,
+CREATE TABLE orders(
+    user_id int REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE,
     product_id int REFERENCES product(id) ON UPDATE CASCADE,
     amount int NOT NULL,
     order_date timestamp default now(),
-    CONSTRAINT user_product_pk PRIMARY KEY (user_id, product_id)
-);
-
-CREATE TABLE g4g_db.file(
-    id serial not null,
-    rec_time timestamp default now(),
-    name varchar,
-    file_code varchar,
-    is_deleted boolean default false
+    PRIMARY KEY (user_id, product_id)
 );
