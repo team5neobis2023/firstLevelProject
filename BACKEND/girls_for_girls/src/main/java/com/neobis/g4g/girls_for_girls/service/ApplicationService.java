14e2ffeaed778fb0ddb163cabc6b6b33dbf350ac
Diyor Umurzakov <diyorp2@mail.ru>
2023-03-08 11:38:09 +0600

    Application updated

--- BACKEND/girls_for_girls/src/main/java/com/neobis/g4g/girls_for_girls/service/ApplicationService.java
+++ BACKEND/girls_for_girls/src/main/java/com/neobis/g4g/girls_for_girls/service/ApplicationService.java
@@ -1,23 +1,28 @@
 package com.neobis.g4g.girls_for_girls.service;
 
 import com.neobis.g4g.girls_for_girls.data.dto.ApplicationDTO;
+import com.neobis.g4g.girls_for_girls.data.entity.Application;
 import com.neobis.g4g.girls_for_girls.repository.ApplicationRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.neobis.g4g.girls_for_girls.data.dto.ApplicationDTO.toApplicationDTO;
 
 @Service
 public class ApplicationService {
     private final ApplicationRepository applicationRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public ApplicationService(ApplicationRepository applicationRepository) {
+    public ApplicationService(ApplicationRepository applicationRepository, ModelMapper modelMapper) {
         this.applicationRepository = applicationRepository;
+        this.modelMapper = modelMapper;
     }
 
 
@@ -33,7 +38,41 @@ public class ApplicationService {
         return new ResponseEntity<>("Application with id " + id + " wasn't found", HttpStatus.NOT_FOUND);
     }
 
-//    public ResponseEntity<?> addApplication(ApplicationDTO applicationDTO) {
-        //TODO
-//    }
+    public ResponseEntity<?> addApplication(ApplicationDTO applicationDTO) {
+        if(applicationRepository.existsByEmail(applicationDTO.getEmail())){
+            return ResponseEntity.badRequest().body("Application with this email already exists");
+        }
+        try {
+            Application application = convertToApplication(applicationDTO);
+            applicationRepository.save(application);
+            return new ResponseEntity<>("Application was created", HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>("Application wasn't created", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    public ResponseEntity<?> updateApplication(Long id, ApplicationDTO applicationDTO){
+        if(applicationRepository.existsById(id)){
+            Application application = convertToApplication(applicationDTO);
+            application.setId(applicationRepository.findById(id).get().getId());
+            applicationRepository.save(application);
+            return ResponseEntity.ok("Application was updated");
+        }else {
+            return new ResponseEntity<>("Application with id " + id + " wasn't found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<?> deleteApplication(Long id){
+        if(applicationRepository.existsById(id)){
+            applicationRepository.deleteById(id);
+            return ResponseEntity.ok("Application was deleted");
+        }else{
+            return new ResponseEntity<>("Application with id " + id + " wasn't found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private Application convertToApplication(ApplicationDTO applicationDTO) {
+        return modelMapper.map(applicationDTO, Application.class);
+    }
 }
