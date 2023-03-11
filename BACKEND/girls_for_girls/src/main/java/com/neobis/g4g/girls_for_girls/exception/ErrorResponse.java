7229b25eda3e0ca781024d45e1df7a767fd8b4b4
Diyor Umurzakov <diyorp2@mail.ru>
2023-03-12 00:56:54 +0600

    Added exceptions

---
+++ BACKEND/girls_for_girls/src/main/java/com/neobis/g4g/girls_for_girls/exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.neobis.g4g.girls_for_girls.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
---
+++ BACKEND/girls_for_girls/src/main/java/com/neobis/g4g/girls_for_girls/exception/NotAddedException.java
@@ -0,0 +1,7 @@
+package com.neobis.g4g.girls_for_girls.exception;
+
+public class NotAddedException extends RuntimeException{
+    public NotAddedException(String msg){
+        super(msg);
+    }
+}
