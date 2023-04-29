81dbcd7e07793f0533dc608aa7d3aafddc2b71be
Diyor Umurzakov <diyorp2@mail.ru>
2023-04-29 17:21:44 +0600

    updated orders

--- BACKEND/girls_for_girls/src/main/java/com/neobis/g4g/girls_for_girls/repository/ProductRepo.java
+++ BACKEND/girls_for_girls/src/main/java/com/neobis/g4g/girls_for_girls/repository/ProductRepo.java
@@ -2,6 +2,7 @@ package com.neobis.g4g.girls_for_girls.repository;
 
 import com.neobis.g4g.girls_for_girls.data.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
